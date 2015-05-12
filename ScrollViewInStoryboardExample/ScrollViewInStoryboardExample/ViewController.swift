4e2b9b403cba0b98789d43ac001059a7db54867a
Rose Schiffler <rose.schiffler@gmail.com>
2015-05-12 20:20:45 +1000

    Added scrollview, content view, text fields, all required constraints and logic when the keyboard is shown to allow for extra scrolling

--- ScrollViewInStoryboardExample/ScrollViewInStoryboardExample/ViewController.swift
+++ ScrollViewInStoryboardExample/ScrollViewInStoryboardExample/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil)
+        
+        var gesture = UITapGestureRecognizer(target: self, action: "scrollViewTouched")
+        scrollView.addGestureRecognizer(gesture)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        self.scrollView.contentSize.height += keyboardSize.height
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.scrollView.contentSize.height = self.contentView.frame.height
+    }
+    
+    func scrollViewTouched() {
+        self.view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
